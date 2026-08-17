129ba8c95f80c989f4a59adfeb672fd191fa4aa3
brainwhat <facelessbeach@gmail.com>
2026-08-17 12:12:53 +0300

    Replace Neovim Markdown renderer

--- dot_config/nvim/lua/custom/plugins/init.lua
+++ dot_config/nvim/lua/custom/plugins/init.lua
@@ -1,8 +1,12 @@
 return {
   {
-    'MeanderingProgrammer/render-markdown.nvim',
-    ft = { 'markdown' },
-    dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' },
-    opts = {},
+    'delphinus/md-render.nvim',
+    version = '*',
+    cmd = 'MdRender',
+    keys = {
+      { '<leader>mp', '<Plug>(md-render-preview)', desc = 'Markdown [P]review' },
+      { '<leader>ms', '<Plug>(md-render-split)', desc = 'Markdown [S]plit preview' },
+      { '<leader>mt', '<Plug>(md-render-toggle)', desc = 'Markdown render [T]oggle' },
+    },
   },
 }
