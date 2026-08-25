7d99596f553490b0f67075c3471d4346fdea9612
brainwhat <facelessbeach@gmail.com>
2026-08-25 14:28:54 +0300

    Restore native Neovim s command

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -931,10 +931,20 @@ require('lazy').setup({
 
       -- Add/delete/replace surroundings (brackets, quotes, etc.)
       --
-      -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
-      -- - sd'   - [S]urround [D]elete [']quotes
-      -- - sr)'  - [S]urround [R]eplace [)] [']
-      require('mini.surround').setup()
+      -- - gsaiw) - [G]o [S]urround [A]dd [I]nner [W]ord [)]Paren
+      -- - gsd'   - [G]o [S]urround [D]elete [']quotes
+      -- - gsr)'  - [G]o [S]urround [R]eplace [)] [']
+      require('mini.surround').setup {
+        mappings = {
+          add = 'gsa',
+          delete = 'gsd',
+          find = 'gsf',
+          find_left = 'gsF',
+          highlight = 'gsh',
+          replace = 'gsr',
+          update_n_lines = 'gsn',
+        },
+      }
 
       -- Simple and easy statusline.
       --  You could remove this setup call if you don't like it,
